cef4ae0389a5b58f75646353dbe995e2db4e8795
jansoft54 <Hdt.tec@gmail.com>
2017-12-05 13:26:37 +0100

    Update Day5.java

--- src/Day5.java
+++ src/Day5.java
@@ -1,20 +1,28 @@
 import java.util.ArrayList;
+import static java.lang.System.out;
 
 public class Day5 {
     public static void main(String... msrgs) {
         ArrayList<String> instructions = IO.ReadLine("FILENAME");
-        int[] nums = new int[instructions.size()];
+        int[] nums0 = new int[instructions.size()];
+        int[] nums1 = new int[instructions.size()];
         for (int i = 0; i < instructions.size(); i++) {
-            nums[i] = Integer.parseInt(instructions.get(i));
+            nums0[i] = Integer.parseInt(instructions.get(i));
+            nums1[i] = Integer.parseInt(instructions.get(i));
         }
+        compute(nums0,Integer.MAX_VALUE,"one");
+        compute(nums1,3,"two");
+    }
+
+    private static void compute(int[] nums, int size, String part) {
         int counter = 0, current = 0, jump = 0;
         for (; ; ) {
             counter++;
             jump = nums[current];
             if (current + jump >= nums.length) break;
-            nums[current] += nums[current] >= 3 ? -1 : 1;
+            nums[current] += nums[current] >= size ? -1 : 1;
             current += jump;
         }
-        System.out.println("counter" + counter);
+        out.printf("%s: %s", part, counter);
     }
 }
